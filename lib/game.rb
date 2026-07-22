f32cd92d943569f6aede2bfbbb0679c7f6196372
Ridanm <ridanm@hotmail.com>
2026-07-22 07:43:07 -0300

    Starting the class

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+# frozen_sgring_literal: true
+
+# This class implements the game in its development.
+class Game
+  def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+  end
+end
