3be7bec3bd627f2fafcfb3ad2c5495777bd1cff4
Ridanm <ridanm@hotmail.com>
2026-07-22 07:46:06 -0300

    Created #enter_piece_color and #create_player methods

--- lib/game_settings.rb
+++ lib/game_settings.rb
@@ -10,13 +10,24 @@ class GameSettings
     print Info.show('enter_name')
     name = gets.chomp.capitalize.strip.squeeze(' ')
     enter_name if name.empty?
+    name
   end
 
   def enter_piece_color
-
+    print Info.show('select piece color')
+    color = gets.chomp.to_i
+    return color if color.between?(1, 2)
+    enter_piece_color
   end
 
   def create_player
-
+    name = enter_name
+    color = enter_piece_color
+    if player_color == 1
+      'white'
+    elsif player_color == 2
+      'black'
+    end
+    Player.new(name, color)
   end
 end
