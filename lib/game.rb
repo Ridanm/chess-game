f43f0e071a7e888fd64748bb984af6dfe1ed3d03
Ridanm <ridanm@hotmail.com>
2026-07-25 08:50:24 -0300

    #initialize the game

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,12 @@
-# frozen_sgring_literal: true
+# frozen_string_literal: true
 
+require_relative 'board'
 # This class implements the game in its development.
 class Game
+  attr_reader :board, :player_one, :player_two
+  
   def initialize(player_one, player_two)
+    @board = Board.new
     @player_one = player_one
     @player_two = player_two
   end
