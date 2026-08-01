7e602e323ec05d33f43f2b0b9303ed305f02bd2b
Ridanm <ridanm@hotmail.com>
2026-08-01 09:04:09 -0300

    Test added

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+require_relative '../lib/game_settings'
+
 RSpec.describe Player do
+  let(:player_one) { Player.new('Jhon', 'black') }
   
+  describe 'player' do
+    it 'if we give it a name' do
+      expect(player_one.name).to eq('Jhon')
+    end
+
+    it 'which color did he choose?' do
+      expect(player_one.piece_color).to eq('black')
+    end
+  end
 end
