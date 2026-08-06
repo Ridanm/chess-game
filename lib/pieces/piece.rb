0b07380a6871e9e628e37bf76e97003fc8795f72
Ridanm <ridanm@hotmail.com>
2026-08-06 07:53:08 -0300

    Added methods #valid_moves, #move_to, #opponent_color

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -3,7 +3,7 @@
 require_relative 'pieces_module'
 # This is a base class for the pieces which will share color and position.
 class Piece
-  attr_reader :color
+  attr_reader :color, :has_moved
   attr_accesor :position
 
   include Pieces
@@ -11,6 +11,7 @@ class Piece
   def initialize(color, position)
     @color = color
     @position = position
+    @has_moved = false
   end
 
   def symbol
@@ -21,7 +22,21 @@ class Piece
     symbol
   end
 
-  def possible_moves(board)
-    raise NotImplementedError, 'Implrment #possible_moves in a subclass'
+  def valid_moves(board)
+    raise NotImplementedError, 'Implement #valid_moves in a subclase'
+  end
+
+  def move_to(new_position, board)
+    raise NotImplementedError, 'Implrment #move_to in a subclass'
+    @has_moved = true
+    @position = new_position
+  end
+
+  def opponent_color
+    @color == 'white' ? 'black' : 'white'
+  end
+
+  def clone
+    # to simulate movements
   end
 end
