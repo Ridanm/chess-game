e252e7b25351cbeeb877198a7c63a4469845d73e
Ridanm <ridanm@hotmail.com>
2026-08-06 07:55:46 -0300

    Initial test

---
+++ spec/piece_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe Piece do
+  describe '#initialize' do
+    context 'when creating a white piece' do
+      before do
+        let(:white_pawn) { Piece.new('white', 'a2') }
+      end
+      it 'any piece' do
+        
+      end
+    end
+  end
+end
