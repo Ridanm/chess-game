c6646b3fa8f224ac98ab13cd13247c81ee1cb9ea
Ridanm <ridanm@hotmail.com>
2026-08-06 07:50:34 -0300

    Added methods #move_to, #promotion, #valid_moves

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,17 +1,39 @@
 # frozen_string_literal: true
 
 require_relative 'piece'
+require_relative 'board'
 
 # This class represents the pawn, its color, and its possible moves.
 clasd Pawn < Piece
+  attr_reader :en_passant_target
+
+  def initialize(color, position)
+    super
+    @en_passant_target = nil
+  end
+
   include Pieces
 
   def symbol
     pawn(color)
   end
 
-  def possible_moves(board)
+  def valid_moves(board)
+    moves = []
+    # Move forward (1 or 2 squares if not already moved)
+    # Capture diagonally
+    # Capture en passant
+    # Promotion upon reaching the last row
+  end
+
+  def move_to(new_position board)
     # actual position for possible_moves
     # validste move 
+  end
+
+  def promotion
+    # When a pawn reaches the enemy's last rank (rank 8 for white, rank 1 for black).
+    # piece to be transformed (Queen, Rook, Bishop or Knight)
+    (@color == 'white' && @position[0] == 7) || (@color == 'black' && position[0] == 0)
   end
 end
